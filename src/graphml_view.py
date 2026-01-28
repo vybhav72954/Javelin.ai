28f0acd55fa97863a4df2b005953454943203fca
Vybhav <vybhavchaturvedi@gmail.com>
2026-01-28 08:22:25 +0530

    Removed old /src scirpts - cleaned the tree

--- src/graphml_view.py
+++
@@ -1,9 +0,0 @@
-import networkx as nx
-import matplotlib.pyplot as plt
-
-# Load the graph from file
-G = nx.read_graphml("../outputs/knowledge_graph.graphml")
-
-# Draw the graph
-nx.draw(G, with_labels=True)
-plt.show()
